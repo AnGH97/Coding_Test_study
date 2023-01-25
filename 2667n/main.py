dfd077ad783e704a9439325ed369ce81dd3f2696
AnGH97 <116076824+AnGH97@users.noreply.github.com>
2023-01-25 22:19:03 +0900

    백준 2667번
    dfs, bfs 모두 가능한 문제
    bfs로 풀이 하였음
    전형적인 bfs문제로 상하좌우에 대한 개념과 나누는 것에 대한 개념에 대해 익힘

---
+++ 2667n/main.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+N = int(input())
+graph = []
+res = []
+
+for _ in range(N):
+    g = list(map(int, input()))
+    graph.append(g)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(graph, a, b):
+    queue = deque()
+    queue.append([a, b])
+    graph[a][b] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        graph[x][y] = 0
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph):
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append([nx, ny])
+                count+=1
+    return count
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            count = bfs(graph, i, j)
+            res.append(count)
+
+res.sort()
+
+print(len(res))
+for k in res:
+    print(k)
+
+
